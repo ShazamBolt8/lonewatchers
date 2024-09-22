fc7143a288532723873bdd6845a4a3cd31ee0dc3
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-09-22 18:19:33 +0500

    wrote Realm looping system

---
+++ src/system/Loop.js
@@ -0,0 +1,26 @@
+import { Clock } from "../../vendor/three/three.module.js";
+class Loop {
+  constructor(camera, scene, renderer) {
+    this.camera = camera;
+    this.scene = scene;
+    this.renderer = renderer;
+    this.updatables = [];
+    this.clock = new Clock();
+  }
+  start() {
+    this.renderer.setAnimationLoop(() => {
+      this.tick();
+      this.renderer.render(this.scene, this.camera);
+    });
+  }
+  stop() {
+    this.renderer.setAnimationLoop(null);
+  }
+  tick() {
+    const delta = this.clock.getDelta();
+    for (const object of this.updatables) {
+      object.tick(delta);
+    }
+  }
+}
+export { Loop };
