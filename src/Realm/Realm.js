6cde3e9099217c81099bdc30d301cb763d497249
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-09-22 19:55:26 +0500

    imported all the created files into realm

--- src/Realm/Realm.js
+++ src/Realm/Realm.js
@@ -1,3 +1,9 @@
+import { createScene } from "../component/scene.js";
+import { createCamera } from "../component/camera.js";
+import { createRenderer } from "../system/renderer.js";
+import { createCube } from "../component/cube.js";
+import { Resizer } from "../system/Resizer.js";
+import { Loop } from "../system/Loop.js";
 class Realm {
   constructor(container) {
     this.container = container;
