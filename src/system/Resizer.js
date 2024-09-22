134a66164e6cf27362d29b6b7c56f88483196514
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-09-22 18:13:27 +0500

    wrote a responsive resizing system

---
+++ src/system/Resizer.js
@@ -0,0 +1,18 @@
+class Resizer {
+  constructor(container, camera, renderer) {
+    this.container = container;
+    this.camera = camera;
+    this.renderer = renderer;
+    this.setSize();
+    window.addEventListener("resize", () => {
+      this.setSize();
+    });
+  }
+  setSize() {
+    this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+  }
+}
+export { Resizer };
