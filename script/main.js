b0d9e30a8dd05f00ab0a1be9debe06e62f654a34
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-09-22 17:39:00 +0500

    added realm to main.js

--- script/main.js
+++ script/main.js
@@ -1,5 +1,9 @@
+import { Realm } from "../src/Realm/Realm.js";
 async function main() {
-  let container = document.getElementById("scene-container");
+  const container = document.getElementById("scene-container");
+  const realm = new Realm(Realm);
+  await realm.init();
+  realm.start();
 }
 
 main().catch((err) => {
