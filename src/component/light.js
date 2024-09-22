2eb648da35c3bcee25342ed0e17f35885cf9a0c6
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-09-22 20:15:30 +0500

    wrote lights

---
+++ src/component/light.js
@@ -0,0 +1,14 @@
+import { AmbientLight, DirectionalLight, HemisphereLight } from "/vendor/three/three.module.js";
+function ambientLight(intensity = 1, color = 0xffffff) {
+  const light = new AmbientLight(color, intensity);
+  return light;
+}
+function directLight(intensity = 1, color = 0xffffff) {
+  const light = new DirectionalLight(color, intensity);
+  return light;
+}
+function hemisphereLight(intensity = 1, sky = "skyblue", gnd = "brown") {
+  const light = new HemisphereLight(sky, gnd, intensity);
+  return light;
+}
+export { ambientLight, directLight, hemisphereLight };
