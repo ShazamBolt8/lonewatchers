e812bb4cf4057a3bdce8386637e94cfef4444820
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-09-22 20:07:59 +0500

    made a basic realm

--- src/Realm/Realm.js
+++ src/Realm/Realm.js
@@ -4,13 +4,33 @@ import { createRenderer } from "../system/renderer.js";
 import { createCube } from "../component/cube.js";
 import { Resizer } from "../system/Resizer.js";
 import { Loop } from "../system/Loop.js";
+
 class Realm {
   constructor(container) {
     this.container = container;
+    this.camera = createCamera();
+    this.scene = createScene();
+
+    //add a basic cube
+    this.cube = createCube();
+    this.cube.rotateY(2); //for perspective
+    this.scene.add(this.cube);
+
+    this.renderer = createRenderer();
+    this.container.append(this.renderer.domElement);
+
+    this.loop = new Loop(this.camera, this.scene, this.renderer);
+    this.reszer = new Resizer(this.container, this.camera, this.renderer);
   }
   async init() {}
-  render() {}
-  start() {}
-  stop() {}
+  render() {
+    this.renderer.render(this.scene, this.camera);
+  }
+  start() {
+    this.loop.start();
+  }
+  stop() {
+    this.loop.stop();
+  }
 }
 export { Realm };
