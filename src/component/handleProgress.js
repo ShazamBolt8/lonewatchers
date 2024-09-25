d304d079aa21de6c17f0dab6552ec8a3074ab6eb
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-09-25 21:13:59 +0500

    added progress handler

---
+++ src/component/handleProgress.js
@@ -0,0 +1,7 @@
+function handleProgress(xhr, message = "") {
+  if (xhr.lengthComputable) {
+    const progress = (xhr.loaded / xhr.total) * 100;
+    const log = `${message} ${Math.round(progress, 2)}%`;
+    console.log(log.trim());
+  }
+}
