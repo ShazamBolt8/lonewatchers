e70e0e83dc165332b552e9bdf2148b4c655a6a3f
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-10-11 23:03:50 +0500

    debugging progress logger

--- src/component/handleProgress.js
+++ src/component/handleProgress.js
@@ -1,10 +1,11 @@
 function handleProgress(xhr, message = "") {
   if (xhr.lengthComputable) {
+    console.log("Loaded:", xhr.loaded);
+    console.log("Total:", xhr.total);
     const logBar = document.getElementById("logger");
     const progress = (xhr.loaded / xhr.total) * 100;
     const log = `${message} ${Math.round(progress, 2)}%`;
     logBar.textContent = log;
-    console.log(progress);
     if (progress >= 100) {
       setTimeout(() => {
         logBar.textContent = "";
