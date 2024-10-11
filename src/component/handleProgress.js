813a1256955e49de7a93a52b1f59e8c7e3d9273b
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-10-11 23:13:56 +0500

    minor bug fixes

--- src/component/handleProgress.js
+++ src/component/handleProgress.js
@@ -1,12 +1,10 @@
 function handleProgress(xhr, message = "") {
   if (xhr.lengthComputable) {
-    console.log("Loaded:", xhr.loaded);
-    console.log("Total:", xhr.total);
     const logBar = document.getElementById("logger");
     const progress = (xhr.loaded / xhr.total) * 100;
     const log = `${message} ${Math.round(progress, 2)}%`;
     logBar.textContent = log;
-    if (progress >= 100) {
+    if (progress >= 200) {
       setTimeout(() => {
         logBar.textContent = "";
       }, 2000);
